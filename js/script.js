2ff7ed02ee818f3b9dab38b375c6bbd0d337eee0
Nate Mueller <natemueller2@gmail.com>
2022-02-24 15:50:42 -0500

    cleaned up code and added a README.md

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-/*----- constants -----*/
+
 const suits = ['h', 's', 'd', 'c']
 const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A']
 
-/*----- app's state (variables) -----*/
 let cards = []
 let playerHand = []
 let cpuHand = []
@@ -10,33 +9,37 @@ let playerCardValue = 'back-blue'
 let cpuCardValue = 'back-red'
 let playerWar = []
 let cpuWar = []
-/*----- cached element references -----*/
+
 const drawEl = document.getElementById('draw-card-button')
 const restartEl = document.getElementById('restart-card-button')
 const videoButtonEl = document.getElementById('videoButton')
 const videoEl = document.getElementById('video')
-
 const soundEl = document.getElementById('sound')
-
 const playerCardEl = document.getElementById('player-card')
 const cpuCardEl = document.getElementById('cpu-card')
-
 const playerCardAmount = document.getElementById('player-tally')
 const cpuCardAmount = document.getElementById('cpu-tally')
-
 const PlayerFlippedWarCard = document.getElementById('inPlayWarPlayer-card')
 const cpuFlippedWarCard = document.getElementById('inPlayWarCpu-card')
 const warCpuCards = document.querySelectorAll('#warCpu-card')
 const warPlayerCards = document.querySelectorAll('#warPlayer-card')
-/*----- event listeners -----*/
+
 drawEl.addEventListener('click', handleClick)
-
 restartEl.addEventListener('click', startUp)
-
 videoButtonEl.addEventListener('click', closeInstructions)
 
-/*----- functions -----*/
 startUp()
+
+function startUp(){
+    initializeValues()
+    createCards()
+    shuffleCards()
+    dealCards()
+    displayCard()
+    displayCardAmount()
+    hideWarCards()
+    startUpResults()
+}
 
 function initializeValues(){
     cards = []
@@ -48,26 +51,9 @@ function initializeValues(){
     cpuCardValue = 'back-red'
     drawEl.disabled = false
 }
-function flipNoise(){
-    soundEl.play()
-}
 
-function startUp(){
-    initializeValues()
-    createCards()
-    shuffleCards()
-    dealCards()
-    displayCard()
-    displayCardAmount()
-    hideWarCards()
-    console.log(playerCardValue)
-    testWinViaWar()
-    startUpResults()
-    
 
-    // warTestDealDeck()
-    // testWinDeck()
-}
+
 
 function createCards(){
     for(const suit of suits){
@@ -90,7 +76,6 @@ function dealCards(){
         }
     }
 }
-console.log(playerHand)
 
 function startUpResults(){
     document.getElementById('result').innerText = "Who will win the battle?"
@@ -103,7 +88,6 @@ function handleClick(){
     compareCards()
     displayCardAmount()
     checkWin()
-    console.log("end of click")
     console.log(playerHand)
     console.log(cpuHand)
 }
@@ -122,7 +106,6 @@ function checkWin(){
 
 function publishResults(results){
     document.getElementById('result').innerText = results
-    console.log(results)
 }
 
 function drawCards(){
@@ -135,53 +118,42 @@ function displayCard(){
     cpuCardEl.className = `card xlarge ${cpuCardValue}`
 }
 
+function displayCardAmount(){
+    playerCardAmount.innerText = `Player Card Amount: ${playerHand.length}`
+    cpuCardAmount.innerText = `CPU Card Amount: ${cpuHand.length}`
+}
+
 function compareCards(){
     if(values.indexOf(playerCardValue.substring(1)) > values.indexOf(cpuCardValue.substring(1))){
-        playerHand.push(playerCardValue)
-        playerHand.push(cpuCardValue)
-        playerHand = playerHand.concat(playerWar)
-        playerHand = playerHand.concat(cpuWar)
-        console.log(playerHand)
-        hideWarCards()
-        playerWar = []
-        cpuWar = []
+        fillHand(playerHand)
 
     } else if (values.indexOf(playerCardValue.substring(1)) < values.indexOf(cpuCardValue.substring(1))){
-        cpuHand.push(playerCardValue)
-        cpuHand.push(cpuCardValue)
-        cpuHand = cpuHand.concat(playerWar)
-        cpuHand = cpuHand.concat(cpuWar)
-        hideWarCards()
-        playerWar = []
-        cpuWar = []
-        console.log(cpuHand)
+        fillHand(cpuHand)
     }else{
         warEvent()
     }
 }
 
+function fillHand(hand){
+    hand.push(playerCardValue)
+    hand.push(cpuCardValue)
+    playerWar.forEach((card) => hand.push(card))
+    cpuWar.forEach((card) => hand.push(card))
+    hideWarCards()
+    playerWar = []
+    cpuWar = []
+}
+
 function warEvent(){
     if(playerHand.length < 4){
-        cpuHand.push(playerCardValue)
-        cpuHand.push(cpuCardValue)
-        cpuHand = cpuHand.concat(playerWar)
-        cpuHand = cpuHand.concat(cpuWar)
-        hideWarCards()
-        playerWar = []
-        cpuWar = []
+        fillHand(cpuHand)
         cpuHand = cpuHand.concat(playerHand)
         playerHand = []
     } else if(cpuHand.length < 4){
-        playerHand.push(playerCardValue)
-        playerHand.push(cpuCardValue)
-        playerHand = playerHand.concat(playerWar)
-        playerHand = playerHand.concat(cpuWar)
-        hideWarCards()
-        playerWar = []
-        cpuWar = []
+        fillHand(playerHand)
         playerHand = playerHand.concat(cpuHand)
         cpuHand = []
-    }else {
+    }else{
         playWar()
     }
 }
@@ -192,10 +164,8 @@ function playWar(){
     for(let i=0; i < 3; i++){
         playerWar.push(playerHand.shift())
         cpuWar.push(cpuHand.shift())
-        
     }
     displayWarCards()
-    console.log(playerWar)
 }
 
 function displayWarCards(){
@@ -208,31 +178,11 @@ function hideWarCards(){
     warPlayerCards.forEach((element) => element.style.display = 'none')
 }
 
-function displayCardAmount(){
-    playerCardAmount.innerText = `Player Card Amount: ${playerHand.length}`
-    cpuCardAmount.innerText = `CPU Card Amount: ${cpuHand.length}`
-    console.log("working")
+function flipNoise(){
+    soundEl.play()
 }
 
 function closeInstructions(){
     videoEl.remove()
     videoButtonEl.remove()
-}
-
-function warTestDealDeck(){
-    cpuHand = ['s07', 's09', 'd07', 'sJ', 'c10', 'hJ', 'c03', 'cA', 's02', 'd08', 'cK', 'dA', 'h03', 'h07', 'c04', 'd09',  'h05', 'h06', 'c05', 'cJ', 'd06', 'sQ', 's04', 'dJ', 'd03', 'dQ']
-
-    playerHand = ['h02', 'd02', 'c07', 'cQ', 'h09', 'd10', 's03', 'd05', 'hA', 's08', 'hK', 'hQ', 'c02', 'sA', 's10', 'sK', 's06', 'dK', 'd04',  'h10', 'h04', 'c08', 'c09', 'c06', 'h08', 's05']
-}
-
-function testWinDeck(){
-    playerHand = ['s07', 's09', 'c07', 'cQ', 'h09', 'd10', 's03', 'd05', 'hA', 's08', 'hK', 'hQ', 'c02', 'c09','d04', 'sK', 's06', 'dK', 's05', 's10',  'h10', 'h04', 'c08', 'sA' , 'c06', 'dQ']
-
-    cpuHand = ['h02', 'd02', 'd07', 'sJ', 'c10', 'hJ', 'c03', 'cA', 's02', 'd08', 'cK', 'dA', 'h03', 'h07', 'c04', 'd09',  'h05', 'h06', 'c05', 'cJ', 'd06', 'sQ', 's04', 'dJ', 'd03', 'h08']
-}
-
-function testWinViaWar(){
-    playerHand = ['s07', 's09', 'c07', 'cQ', 'h09', 'd10', 's03', 'd05', 'hA', 's08', 'hK', 'hQ', 'c02', 'c09','d04', 'sK', 's06', 'dK', 's05', 's10',  'h10', 'h04', 'sA' , 'c06', 'dQ', 'c08']
-
-    cpuHand = ['h02', 'd02', 'd07', 'sJ', 'c10', 'hJ', 'c03', 'cA', 's02', 'd08', 'cK', 'dA', 'h03', 'h07', 'c04', 'd09',  'h05', 'h06', 'c05', 'cJ', 'd06', 'sQ', 's04', 'dJ', 'd03', 'h08']
 }
